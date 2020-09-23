a5b1107478e29e8ea89e44a7a55d758ff48384b9
badiushkni <iulian.zadnipru@gmail.com>
2020-09-23 10:41:05 +0300

    Add New Markets

--- src/components/MarketList.js
+++ src/components/MarketList.js
@@ -1,8 +1,104 @@
 import React from "react";
-// import { Loading, Card, Icon, Tag } from "element-react";
+import { graphqlOperation } from "aws-amplify";
+import { Connect } from "aws-amplify-react";
+import { listMarkets } from "../graphql/queries";
+import { onCreateMarket } from "../graphql/subscriptions";
+import { Loading, Card, Icon, Tag } from "element-react";
+import { Link } from "react-router-dom";
+import Error from "./Error";
 
 const MarketList = () => {
-  return <div>MarketList</div>;
+  const onNewMarket = (prevQuery, newData) => {
+    let updatedQuery = { ...prevQuery };
+    const updatedMarketList = [
+      newData.onCreateMarket,
+      ...prevQuery.listMarkets.items,
+    ];
+    updatedQuery.listMarkets.items = updatedMarketList;
+    return updatedQuery;
+  };
+
+  return (
+    <Connect
+      query={graphqlOperation(listMarkets)}
+      subscription={graphqlOperation(onCreateMarket)}
+      onSubscriptionMsg={onNewMarket}
+    >
+      {({ data, loading, errors }) => {
+        if (errors.length > 0) return <Error errors={errors} />;
+        if (loading || !data.listMarkets) return <Loading fullscreen={true} />;
+
+        return (
+          <>
+            <h2>
+              <svg
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                className="bi bi-shop"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                style={{ paddingRight: "0.5em" }}
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"
+                />
+              </svg>
+              Markets
+            </h2>
+            {data.listMarkets.items.map((market) => (
+              <div key={market.id} className="my-2">
+                <Card
+                  bodyStyle={{
+                    padding: "0.7em",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <div>
+                    <span className="flex">
+                      <Link className="link" to={`/markets/${market.id}`}>
+                        {market.name}
+                      </Link>
+                      <span style={{ color: "var(--darkAmazonOrange)" }}>
+                        {/* {market.products.items.length} */}
+                      </span>
+                      <svg
+                        width="1em"
+                        height="1em"
+                        viewBox="0 0 16 16"
+                        className="bi bi-cart-fill"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+                        />
+                      </svg>
+                    </span>
+                    <div style={{ color: "var(--lightSquidInk)" }}>
+                      {market.owner}
+                    </div>
+                  </div>
+                  <div>
+                    {market.tags &&
+                      market.tags.map((tag) => (
+                        <Tag key={tag} type="danger" className="mx-1">
+                          {tag}
+                        </Tag>
+                      ))}
+                  </div>
+                </Card>
+              </div>
+            ))}
+          </>
+        );
+      }}
+    </Connect>
+  );
 };
 
 export default MarketList;
